59892871731463e361fb0dec71033ff2b8ba01f8
Josh Riddle <jriddle@ksu.edu>
2023-10-28 21:43:03 -0500

    first push

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroceryProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SelectImage(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Document"; // Default file name
+            dialog.DefaultExt = ".txt"; // Default file extension
+            dialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)| *.jpg; *.jpeg; *.gif; *.bmp";
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.FileName;
+                TextImager ts = new();
+                var text = ts.ConvertImageToText(filename);
+                richText.Text = text;
+            }
+        }
+    }
+}
---
+++ TextImager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronOcr;
+
+namespace GroceryProject
+{
+    class TextImager
+    {
+        public string ConvertImageToText(string pathName)
+        {
+            IronTesseract IronOcr = new IronTesseract();
+            using (var input = new OcrInput(pathName))
+            {
+                input.Scale(200);
+                input.Sharpen();
+                input.Contrast();
+                var Result = IronOcr.Read(input);
+                return Result.Text;
+            }
+            
+        }
+    }
+}
